498f7ad63d6c71668f8388fb352255461df95df6
admapop <52109936+admapop@users.noreply.github.com>
2019-08-31 14:26:24 +0200

    Update server.js
    removed local database

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.json(database.users)})
+app.get('/', (req, res) => { res.send('Hooray!')})
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 
